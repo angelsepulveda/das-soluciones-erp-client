250bdbecce2ca1131729a53bfc1cbd0197f22677
Aangelsepulveda <angelsepulvedasep@gmail.com>
2025-01-19 16:59:02 -0300

    correciones namespaces

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router';
 import App from '../App.tsx';
-import { PrivateLayout } from '../Layouts';
+import { PrivateLayout } from '../layouts';
 import { ModulesContainer } from '../pages';
 
 export const router = createBrowserRouter([
